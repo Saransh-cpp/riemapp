1b86141ed23b9495856d61ad724656019729bf83
Naman Priyadarshi <77211855+Naman-Priyadarshi@users.noreply.github.com>
2022-09-26 12:42:08 +0530

    update core.py

--- riemapp/core.py
+++ riemapp/core.py
@@ -2,14 +2,14 @@ from __future__ import annotations
 
 from manim import ApplyComplexFunction, Create, NumberPlane, Scene
 
-from riemapp.geometry import Polygon, Rectangle, RegularPolygon, Square
+from riemapp.geometry import Polygon, Rectangle, RegularPolygon, Square,  Triangle
 
 # from manim.utils.file_ops import open_file as open_media_file
 
 
 class ComplexMap:
     def __init__(
-        self, f: Square | Rectangle | Polygon | RegularPolygon, transformation: callable
+        self, f: Square | Rectangle | Polygon | RegularPolygon | Triangle, transformation: callable
     ):
         self.f = f
         self.transformation = transformation
@@ -32,7 +32,7 @@ class ComplexMap:
     class Animate(Scene):
         def __init__(
             self,
-            f: Square | Rectangle | Polygon | RegularPolygon,
+            f: Square | Rectangle | Polygon | RegularPolygon | Triangle,
             transformation: callable,
             add_numberplane: bool = False,
             run_time: float = 1.0,
