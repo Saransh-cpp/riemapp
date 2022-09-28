5f65b22963206100e75fb61dbc78fc789203f53d
Naman-Priyadarshi <namanpriyadarshicr7@gmail.com>
2022-09-28 15:36:09 +0530

    add triangle_geometry test

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -1,3 +1,10 @@
 from __future__ import annotations
 
-# import riemapp as rp
+import riemapp as rp
+import manim
+
+
+def test_triangle():
+    triangle = rp.geometry.Triangle()
+    assert triangle.__repr__() == "Triangle()"
+    assert isinstance(triangle, (rp.geometry.Triangle, manim.Triangle))
