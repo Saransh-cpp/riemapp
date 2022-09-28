0e0ff14d8185f5269abb1c6eb621e88fdd3101a9
Naman Priyadarshi <77211855+Naman-Priyadarshi@users.noreply.github.com>
2022-09-28 16:28:58 +0530

    add test for square in geometry (#6)
    * add square test
    
    * style: pre-commit fixes
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -9,3 +9,10 @@ def test_triangle():
     triangle = rp.geometry.Triangle()
     assert triangle.__repr__() == "Triangle()"
     assert isinstance(triangle, (rp.geometry.Triangle, manim.Triangle))
+
+
+def test_square():
+    square = rp.geometry.Square(side_length=2.0)
+    assert square.__repr__() == f"Square(side_length={square.side_length})"
+    assert isinstance(square, (rp.geometry.Square, manim.Square))
+    assert square.side_length == 2.0
