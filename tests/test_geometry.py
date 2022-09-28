b5865f3c116a6afba04978c79f7920de887e19bf
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2022-09-28 17:29:43 +0530

    Add test for circle

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -16,3 +16,10 @@ def test_square():
     assert square.__repr__() == f"Square(side_length={square.side_length})"
     assert isinstance(square, (rp.geometry.Square, manim.Square))
     assert square.side_length == 2.0
+
+
+def test_circle():
+    circle = rp.geometry.Circle(radius=1.0)
+    assert circle.__repr__() == f"Circle(radius={circle.radius})"
+    assert isinstance(circle, (rp.geometry.Circle, manim.Circle))
+    assert circle.radius == 1.0
